df2aff1b384a4572284786a9e4a6060676b84e08
Moor.Zhou <40941533+Mudong-17@users.noreply.github.com>
2025-11-25 14:34:23 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -43,12 +43,12 @@ const CONFIG = {
   ],
 
   // 个人资料底部按钮
-  HEO_INFO_CARD_URL1: '/about',
-  HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
+  HEO_INFO_CARD_URL1: 'https://discord.gg/HaH3cgw4TF',
+  HEO_INFO_CARD_ICON1: 'fab fa-discord',
+  HEO_INFO_CARD_URL2: 'https://github.com/Mudong-17',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
-  HEO_INFO_CARD_TEXT3: '了解更多',
+  HEO_INFO_CARD_URL3: '/about',
+  HEO_INFO_CARD_TEXT3: '关于',
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
